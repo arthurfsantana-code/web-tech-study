421563d7460c4a60ed028dd07f3c5ebdef730f53
arthurfsantana-code <arthur.f.santana@edu.senai.br>
2026-06-10 09:04:40 -0300

    Att nos exemplos pt 2

---
+++ form-web/js/calc2.js
@@ -0,0 +1,20 @@
+let num1 = Number(prompt("Digite um número"))
+let num2 = Number(prompt("Digite outro número"))    
+// Number()
+
+
+// operação de soma
+let soma = num1 + num2
+let subtracao = num1 - num2
+let multiplicacao = num1 * num2
+let divisao = num1 / num2
+
+console.log(soma);
+console.log(subtracao);
+console.log(multiplicacao);
+console.log(divisao);
+
+console.log(typeof soma); // number
+console.log(typeof subtracao); // number
+console.log(typeof multiplicacao); // number
+console.log(typeof divisao); // number  
