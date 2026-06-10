264a116381972d31bd3197ec6ead1fe42468375e
arthurfsantana-code <arthur.f.santana@edu.senai.br>
2026-06-10 08:45:37 -0300

    Att nos exemplos do JV

---
+++ form-web/js/func.js
@@ -0,0 +1,20 @@
+document.write("Olá Mundo!");
+function minhaFuncao() {
+    document.write("Minha Função!");
+}
+
+function pularLinha() {
+    document.write("<br><br>");
+}
+
+function mostrarNome() {
+   document.write("Arthur)");
+    pularLinha();
+
+}
+
+minhaFuncao();
+pularLinha();
+minhaFuncao();
+mostrarNome(Arthur);
+mostrarNome(Spike);
